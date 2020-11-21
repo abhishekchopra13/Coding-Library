6aef10342810cd910fb43fb062b142711bffa0eb
Abhishek Chopra <40965777+abhishekchopra13@users.noreply.github.com>
2020-11-22 00:51:24 +0530

    Add files via upload

---
+++ fft.cpp
@@ -0,0 +1,140 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#include "bits/stdc++.h"
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+//using namespace __gnu_pbds;
+#define int long long int
+#define SYNC std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define FRE freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<int> vi;
+typedef vector<ii>   vii;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,
+//             tree_order_statistics_node_update>
+//    ost;
+#define rep(i,l,r)   for (int i = (l); i < (r); i++)
+#define here cout << " Hey!!\n";
+#define  pb push_back
+#define  F  first
+#define  S  second
+#define all(v) (v).begin(),(v).end()
+#define sz(a) (int)((a).size())
+#define sq(x) ((x)*(x))
+template <typename T>
+void etch(T V) {
+    for (auto x : V)    cout << x << ' ' ;
+    cout << '\n';
+}
+vector<string> vec_splitter(string s) {
+    s += ',';
+    vector<string> res;
+    while(!s.empty()) {
+        res.push_back(s.substr(0, s.find(',')));
+        s = s.substr(s.find(',') + 1);
+    }
+    return res;
+}
+void debug_out(
+        vector<string> __attribute__ ((unused)) args,
+        __attribute__ ((unused)) int idx,
+        __attribute__ ((unused)) int LINE_NUM) { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+    if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+    stringstream ss; ss << H;
+    cerr << args[idx] << " = " << ss.str();
+    debug_out(args, idx + 1, LINE_NUM, T...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+const int MOD = 1e9+7;
+const int MOD1 = 998244353;
+const int N = 2e5+5;
+const int INF = 1000111000111000111LL;
+const ld EPS = 1e-12;
+const ld PI = 3.141592653589793116;
+
+using cd = complex<double>;
+
+void fft(vector<cd> & a, bool invert) 
+{
+    int n = a.size();
+    if (n == 1)
+        return;
+
+    vector<cd> a0(n / 2), a1(n / 2);
+    for (int i = 0; 2 * i < n; i++) {
+        a0[i] = a[2*i];
+        a1[i] = a[2*i+1];
+    }
+    fft(a0, invert);
+    fft(a1, invert);
+
+    double ang = 2 * PI / n * (invert ? -1 : 1);
+    cd w(1), wn(cos(ang), sin(ang));
+    for (int i = 0; 2 * i < n; i++) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n/2] = a0[i] - w * a1[i];
+        if (invert) {
+            a[i] /= 2;
+            a[i + n/2] /= 2;
+        }
+        w *= wn;
+    }
+}
+
+vector<int> multiply(vector<int> const& a, vector<int> const& b) 
+{
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+
+
+int32_t main()
+{
+    SYNC
+    int n, x; cin >> n >> x;
+    vector<int> a(n), pre(n+1, 0), fre(n+1, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        a[i] = (a[i] < x) ? 1 : 0;
+    }
+    for (int i = 1; i <= n; i++) {
+        pre[i] = pre[i-1] + a[i-1];
+    }
+    for (int &x : pre)  fre[x]++;
+
+    vector<int> rev_fre = fre, sub_sums, ans(n+1, 0);
+    reverse(all(rev_fre));
+    sub_sums = multiply(fre, rev_fre);
+    // etch(sub_sums);
+    ans[0] = (sub_sums[n] - (n + 1))/2;
+    for (int i = 1; i <= n; i++) {
+        ans[i] = sub_sums[n+i];
+    }
+    etch(ans);
+    return 0;
+}
